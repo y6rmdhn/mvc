b1ecb3eb9f222a0c00cf44d428f2dd7f8de2d5bb
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2026-03-11 12:48:32 +0700

    controller

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+
+class About
+{
+
+    public function index($nama = 'guest')
+    {
+        echo "Hallooo acu $nama";
+    }
+
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
